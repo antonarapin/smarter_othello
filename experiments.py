014a7ba582bfe50f33b468e31bfd1d7d4f4fe26c
hfacey <hfacey@fandm.edu>
2018-12-01 15:00:30 -0500

    modified agent2

--- experiments.py
+++ experiments.py
@@ -1,8 +1,8 @@
 import sys,os
 
 print("\nWeighted Discs Round Robin test\n_______________________________\n")
-for i in range(1,5):
-    for j in range(i,5):
+for i in range(1,4):
+    for j in range(i,4):
         print("\nNow Trying p1 at d=",i,"and p2 at d=",j)
         os.system("python3 playOthello.py -nd -p1 othelloAgent2.py -p2 othelloAgent3.py -p1A "+str(i)+" -p2A "+str(j))
         print("\nNow Trying p1 at d=",j,"and p2 at d=",i)
--- othelloAgent2.py
+++ othelloAgent2.py
@@ -1,26 +1,34 @@
 class OthelloAgent:
     def __init__(self, problem, d=""):
         self.problem = problem
-        self.weights = [[8,1,6,6],[1,0,3,3],[6,3,5,4],[6,3,4,5]]
         #self.weights = [[4,-3,2,2],[-3,-4,-1,-1],[2,-1,1,0],[2,-1,0,1]]
-        #self.weights = [[99,-8,8,6],[-8,-24,-4,-3],[8,-4,7,4],[6,-3,4,0]]
+        self.weights = [[99,-8,8,6],[-8,-24,-4,-3],[8,-4,7,4],[6,-3,4,0]]
         if d=="":
             self.depth = 3
         else:
             self.depth = int(d)
+        self.exploitCheck = False
+        self.cornerList = [(0,0),(7,0),(0,7),(7,7)]
+        self.maxVal = 400
 
     def getMoves(self):
         state = self.problem.getState()
         turn = self.problem.getTurn()
-        moveList,mm = self.MinimaxSearch(state,None,turn,-float('inf'),float('inf'),self.depth)
-
+        self.exploitCheck=True
+        moveList,mm = self.MinimaxSearch(state,turn,-float('inf'),float('inf'),self.depth)
         return moveList
     
-    def MinimaxSearch(self,state,act,turn,a,b,d):
+    def MinimaxSearch(self,state,turn,a,b,d):
         if d<=0:
-            return [], self.weightedGreedyHeuristic(state,act)
+            return [], self.weightedHeuristic(state,turn)
         
         succList = self.problem.getSuccessors(state,turn)
+        if self.exploitCheck:
+            self.exploitCheck=False
+            ex = self.exploits(succList,turn)
+            if len(ex)>0:
+                return ex,0
+
         mmList = [None]*len(succList)
         curMMScore = float('inf')
         if turn == 1:
@@ -33,7 +41,7 @@ class OthelloAgent:
             if succList[i][3]!=None: #base case, terminal state
                 mmList[i] = succList[i][3]/64
             else: 
-                toss,mmList[i] = self.MinimaxSearch(succList[i][0],succList[i][1],succList[i][2],a,b,d-1)
+                toss,mmList[i] = self.MinimaxSearch(succList[i][0],succList[i][2],a,b,d-1)
             if turn==1:
                 if mmList[i]>curMMScore:
                     curMMScore = mmList[i]
@@ -53,33 +61,41 @@ class OthelloAgent:
         
         return [succList[curMMIdx][1]],curMMScore
 
-    def weightedGreedyHeuristic(self,state,action):
-        r,c = action[0],action[1]
-        if r<4:
-            im = r
-        else:
-            im = 3 - r%4
-        if c<4:
-            ij = c
-        else:
-            ij = 3 - c%4
-        counter = 0
-        for row in state:
-            counter+=sum(row)
-        return (self.weights[im][ij]/8)*(counter/64)
     
-    # def weightedHeuristic(self,state):
-    #     score = [0,0]
-    #     for  r in range(8):
-    #         if r<4:
-    #             im = r
-    #         else:
-    #             im = 3 - r%4
-    #         for c in range(8):
-    #             if c<4:
-    #                 ij = c
-    #             else:
-    #                 ij = 3 - c%4
-    #             if state[r][c]!=0:
-    #                 score[(1+state[r][c])//2] += self.weights[im][ij]
-    #     return (score[1]-score[0])/135 #so results are between -1 and 1
+    def weightedHeuristic(self,state,turn):
+        score = [0,0]
+        cScore = list(map(lambda x: state[x[0]][x[1]],self.cornerList))
+        if sum(cScore)!=0:
+            return sum(cScore)/4
+        
+        for  r in range(8):
+            if r<4:
+                im = r
+            else:
+                im = 3 - r%4
+            for c in range(8):
+                if c<4:
+                    ij = c
+                else:
+                    ij = 3 - c%4
+                if state[r][c]!=0:
+                    score[(1+state[r][c])//2] += self.weights[im][ij]
+        val = (score[1]-score[0]) #so results are between -1 and 1
+        self.maxVal = max(self.maxVal,abs(val))
+        return val/self.maxVal
+
+    def exploits(self,succList,turn):
+        corner,block = [],[]
+        for i in range(len(succList)):
+            if succList[i][3]!=None:
+                if (succList[i][3]*turn) > 0: #if you can win right now, do it
+                    return [succList[i][1]]
+            if succList[i][1] in self.cornerList:
+                corner.append(succList[i][1])
+            if succList[i][2]==turn:
+                block.append(succList[i][1])
+        if len(corner)>0:
+            return [corner[0]]
+        if len(block)>0:
+            return [block[0]]
+        return []
