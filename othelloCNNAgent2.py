7b8ebe763adead82d2712bccada68d56218be056
hfacey <hfacey@fandm.edu>
2018-12-01 00:43:07 -0500

    simple cnn

--- othelloCNNAgent2.py
+++ othelloCNNAgent2.py
@@ -1,12 +1,12 @@
 import mlp, numpy as np
 import torch
-from cnn4 import Othello_CNN
+from simple_cnn import Othello_CNN
 
 class OthelloAgent:
     def __init__(self, problem, _=None):
         self.problem = problem
         #self.net = mlp.mlp(weight1File="fstWeights.data",weight2File="sndWeights.data")
-        self.net = torch.load("cnn4_model0.1.pt")
+        self.net = torch.load("simplecnn_model0.05.pt")
         self.net.eval()
 
     def getMoves(self):
