063457dd5be4de0334c2e236bd35d54fed53e02e
ArapinAnton <anton.arapin@cosyrobo.com>
2018-11-30 19:35:14 -0500

    retrained

--- othelloCNNAgent2.py
+++ othelloCNNAgent2.py
@@ -6,7 +6,7 @@ class OthelloAgent:
     def __init__(self, problem, _=None):
         self.problem = problem
         #self.net = mlp.mlp(weight1File="fstWeights.data",weight2File="sndWeights.data")
-        self.net = torch.load("cnn4_model0.08.pt")
+        self.net = torch.load("cnn4_model0.1.pt")
         self.net.eval()
 
     def getMoves(self):
